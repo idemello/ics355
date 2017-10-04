b17c9172387459c40bdfa5532e43900cb1dddda8
idemello <idemello@hawaii.edu>
2017-10-03 17:49:07 -1000

    added currency conversions and output

--- hw1.py
+++ hw1.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyRates, CurrencyCodes
 from forex_python.bitcoin import BtcConverter
+
+c = CurrencyRates()
+sym = CurrencyCodes()
 
 print('Welcome to currency')
 print('Choose a command:\n')
@@ -12,8 +15,17 @@ if choice == 1:
     print('Enter the amount of currency X you would like to convert to currency Y\n');
     print('Example: 1000 USD JPY\n')
     currs = input().split(' ')
-    print(currs)
-    amt = int(currs[0])
+
+    symbol1 = sym.get_symbol(currs[1])
+    symbol2 = sym.get_symbol(currs[2])
+    total = c.convert(currs[1], currs[2], int(currs[0]))
+    
+    print(symbol1  + currs[0] + ' '  + ' is equal to ', end = '')
+    print(symbol2 + str(total) + ' ' + currs[2])
+
 else:
     print('2')
+
+
+
 
